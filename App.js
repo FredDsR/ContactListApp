4669a9288bb5970085505a0e83f5cba0e5f67213
Frederico Reckziegel <freddalsoglio@gmail.com>
2019-06-02 18:12:59 -0300

    passando o método renderList para o componente PeopleList

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import axios from 'axios'
 
 import Header from './src/components/Header';
+import PeopleList from './src/components/PeopleList';
 
 export default class App extends React.Component {
 
@@ -15,31 +16,22 @@ export default class App extends React.Component {
     };
   }
   
-componentDidMount(){
-  axios
-      .get('https://randomuser.me/api/?nat=br&results=5')
-      .then(response => {
-        const {results} = response.data;
-        this.setState({
-          peoples: results
-        });
-      })
-}
-
-  renderList() {
-    const textElements = this.state.peoples.map(people => {
-      const { first } = people.name;
-      return <Text key={ first }>{ first }</Text>; 
-    });
-
-    return textElements;
-  }
+  componentDidMount(){
+    axios
+        .get('https://randomuser.me/api/?nat=br&results=5')
+        .then(response => {
+          const {results} = response.data;
+          this.setState({
+            peoples: results
+          });
+        })
+  } 
 
   render() {
     return (
       <View>
         <Header title='Contacts'/>
-        {this.renderList()}
+        <PeopleList peoples={this.state.peoples} />
       </View>
     );
   }
