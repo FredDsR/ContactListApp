407ca48bcfe463a5f98333b9462fa4f36a8262c1
Frederico Reckziegel <freddalsoglio@gmail.com>
2019-06-02 16:48:40 -0300

    complementando o render list

--- App.js
+++ App.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import axios from 'axios'
+
 import Header from './src/components/Header';
 
 export default class App extends React.Component {
 
+  constructor(props){
+    super(props);
+
+    this.state = {
+        peoples: []
+    };
+  }
+  
+componentDidMount(){
+  axios
+      .get('https://randomuser.me/api/?nat=br&results=5')
+      .then(response => {
+        const {results} = response.data;
+        this.setState({
+          peoples: results
+        })
+      })
+}
+
   renderList() {
-    const names = [
-      'Eddie Van Halen',
-      'Jimi Hendrix',
-      'Chimbinha',
-      'Steve Vai'
-    ];
-
-    const textElements = names.map( name => {
-      return <Text key={name}>{name}</Text>
-    })
-
-    return textElements; 
-
+    
+    // Promise 
+    
+      return 0
   }
 
   render() {
     return (
       <View>
         <Header title='Contacts'/>
-        { this.renderList() }
       </View>
     );
   }
