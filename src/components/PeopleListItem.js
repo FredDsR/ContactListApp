499947bc66ac6016288ab97ae15cb1e8feab77e1
Frederico Reckziegel <freddalsoglio@gmail.com>
2019-06-06 21:42:09 -0300

    Adicionando componente PeopleListItem ao PeopleList

---
+++ src/components/PeopleListItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const PeopleListItem = props => {
+    const {people} = props;
+    const {first, last} = people.name;
+    return (
+        <View style={styles.line}>
+            <Text style={styles.lineText}>
+                { first + ' ' +  last }
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    line: {
+        height: 60,
+        borderBottomWidth: 1,
+        borderBottomColor:"#bbb",
+        
+        alignItems: 'center',
+        flexDirection: 'row',
+
+    },
+    lineText: {
+        fontSize: 20,
+        paddingLeft: 15
+    }
+});
+
+export default PeopleListItem;
